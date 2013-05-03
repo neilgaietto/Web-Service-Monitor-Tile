2bff5a4e420d5805ef09a801527929fafa25dee7
neilgaietto <neil.gaietto@gmail.com>
2013-05-02 23:29:22 -0400

    made tile notification a little better

--- Web-Service-Monitor-Tile/MainPage.xaml.cs
+++ Web-Service-Monitor-Tile/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Navigation;
 using Windows.Data.Json;
 using Windows.Data.Xml.Dom;
 using Windows.UI.Notifications;
+using NotificationsExtensions.TileContent;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -44,29 +45,58 @@ namespace Web_Service_Monitor_Tile
             //http://msdn.microsoft.com/en-us/library/windows/apps/xaml/Hh868253%28v=win.10%29.aspx
             //http://blogs.msdn.com/b/b8/archive/2011/11/02/updating-live-tiles-without-draining-your-battery.aspx
 
-            //square
-            var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquareText01);
+            ////square
+            //var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWideText01);
 
-            var tileTextAttributes = tileXml.GetElementsByTagName("text");
-            tileTextAttributes[0].InnerText = "Web service";
-
-
-            //rec
-            var recTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWideText01);
-            var recTextAttributes = recTemplate.GetElementsByTagName("text");
-            recTextAttributes[0].InnerText = "Web service";
-
-            //append rec to square
-            IXmlNode node = tileXml.ImportNode(recTemplate.GetElementsByTagName("binding").Item(0), true);
-            tileXml.GetElementsByTagName("visual").Item(0).AppendChild(node);
+            //var tileTextAttributes = tileXml.GetElementsByTagName("text");
+            //tileTextAttributes[0].InnerText = "Web service";
 
 
-            //send tile notification
-            TileNotification tileNotification = new TileNotification(tileXml);
-            //tileNotification.ExpirationTime = DateTimeOffset.UtcNow.AddSeconds(10);
+            ////rec
+            //var recTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquareText01);
+            //var recTextAttributes = recTemplate.GetElementsByTagName("text");
+            //recTextAttributes[0].InnerText = "Web service";
 
-            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+            ////append rec to square
+            //IXmlNode node = tileXml.ImportNode(recTemplate.GetElementsByTagName("binding").Item(0), true);
+            //tileXml.GetElementsByTagName("visual").Item(0).AppendChild(node);
 
+
+            ////send tile notification
+            //TileNotification tileNotification = new TileNotification(tileXml);
+            ////tileNotification.ExpirationTime = DateTimeOffset.UtcNow.AddSeconds(10);
+
+            //TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+
+
+            // create the wide template
+            ITileWideText01 tileContent = TileContentFactory.CreateTileWideText01();
+            tileContent.TextHeading.Text = "Web service call";
+            tileContent.TextBody1.Text = "a";
+            tileContent.TextBody2.Text = "blah";
+            tileContent.TextBody3.Text = "c";
+            tileContent.TextBody4.Text = "d";
+            
+            // Users can resize tiles to square or wide.
+            // Apps can choose to include only square assets (meaning the app's tile can never be wide), or
+            // include both wide and square assets (the user can resize the tile to square or wide).
+            // Apps cannot include only wide assets.
+
+            // Apps that support being wide should include square tile notifications since users
+            // determine the size of the tile.
+
+            // create the square template and attach it to the wide template
+            ITileSquareText04 squareContent = TileContentFactory.CreateTileSquareText04();
+            squareContent.TextBodyWrap.Text = "Web service call";
+            tileContent.SquareContent = squareContent;
+
+
+            TileUpdateManager.CreateTileUpdaterForApplication().EnableNotificationQueue(true);
+            // send the notification
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileContent.CreateNotification());
+
+            txtTestResults.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            txtTestResults.Text = tileContent.GetContent();
         }
 
         async private void btnTest_Click(object sender, RoutedEventArgs e)
